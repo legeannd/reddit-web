e144cecdcddb894fefd6716cc3e239f1862703b2
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-25 14:48:01 -0300

    Dynamic navbar showing username if logged in or login and register links if not

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Box, Flex, Link } from '@chakra-ui/core';
+import { Box, Button, Flex, Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
 export const NavBar: React.FC = () => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml="auto">
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link mr={2}>Login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link>Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml="auto">{body}</Box>
     </Flex>
   );
 };
