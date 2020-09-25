789e4e02d628f8d4430c79e7e9d9316c1ec74ac5
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-25 20:16:22 -0300

    Wrapping index with withUrqlClient

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -12,7 +12,7 @@ import { betterUpdateQuery } from './betterUpdateQuery';
 export const createUrqlClient = (ssrExchange: any) => ({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
-    credentials: 'include',
+    credentials: 'include' as const,
   },
   exchanges: [
     dedupExchange,
@@ -62,6 +62,7 @@ export const createUrqlClient = (ssrExchange: any) => ({
         },
       },
     }),
+    ssrExchange,
     fetchExchange,
   ],
 });
