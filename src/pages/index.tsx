0ec76b1c0807f48fe31f07aafa8a6e91308f22e9
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-28 22:44:10 -0300

    Implementing pagination to show posts based on its creation date

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,60 @@
-import { Box, Heading, Link, Stack, Text } from '@chakra-ui/core';
+import { Box, Button, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core';
 import { withUrqlClient } from 'next-urql';
-import React from 'react';
+import React, { useState } from 'react';
 import NextLink from 'next/link';
 import { Layout } from '../components/Layout';
 import { usePostsQuery } from '../generated/graphql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
   });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+  if (!fetching && !data) {
+    return <div>Your query has failed</div>;
+  }
 
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>Create post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">Create post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data.posts.map(p => (
+          {data!.posts.map(p => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
               <Text>{p.textSnippet}</Text>
             </Box>
           ))}
         </Stack>
+      )}
+      {data && (
+        <Flex>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts[data.posts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
+            Load more
+          </Button>
+        </Flex>
       )}
     </Layout>
   );
