c3f50f5d39cba72331a96c68d66bc88dfa508f79
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-29 20:37:02 -0300

    Invalidated cache when create a new post to show it right after creation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { createUrqlClient } from '../utils/createUrqlClient';
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 33,
+    limit: 15,
     cursor: null as null | string,
   });
   const [{ data, fetching }] = usePostsQuery({
@@ -35,6 +35,7 @@ const Index = () => {
           {data!.posts.posts.map(p => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
               <Heading fontSize="xl">{p.title}</Heading>
+              <Text>posted by {p.creator.username}</Text>
               <Text>{p.textSnippet}</Text>
             </Box>
           ))}
