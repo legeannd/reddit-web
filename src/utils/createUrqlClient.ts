d0f7d149773f8e475c031bc4b2d32ce0951d58a8
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-29 20:42:06 -0300

    Invalidating all the queries when create a new post to be sure the data is fresh

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -83,8 +83,12 @@ export const createUrqlClient = (ssrExchange: any) => ({
       updates: {
         Mutation: {
           createPost: (_result, args, cache, info) => {
-            cache.invalidate('Query', 'posts', {
-              limit: 15,
+            const allFields = cache.inspectFields('Query');
+            const fieldInfos = allFields.filter(
+              _info => _info.fieldName === 'posts',
+            );
+            fieldInfos.forEach(fi => {
+              cache.invalidate('Query', 'posts', fi.arguments || {});
             });
           },
           logout: (_result, args, cache, info) => {
