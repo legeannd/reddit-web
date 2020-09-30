cb3a3f2ae854ed9e23a78b639c3356287fc152a3
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-30 19:49:34 -0300

    Changed place of create post button to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,7 +27,12 @@ export const NavBar: React.FC = () => {
     );
   } else {
     body = (
-      <Flex>
+      <Flex align="center">
+        <NextLink href="/create-post">
+          <Button as={Link} mr={4}>
+            Create post
+          </Button>
+        </NextLink>
         <Box mr={2}>{data.me.username}</Box>
         <Button
           onClick={() => {
@@ -44,12 +49,14 @@ export const NavBar: React.FC = () => {
 
   return (
     <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4} align="center">
-      <NextLink href="/">
-        <Link>
-          <Heading>LiReddit</Heading>
-        </Link>
-      </NextLink>
-      <Box ml="auto">{body}</Box>
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>
+            <Heading>LiReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml="auto">{body}</Box>
+      </Flex>
     </Flex>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,13 +31,6 @@ const Index = () => {
 
   return (
     <Layout>
-      <Flex align="center">
-        <Heading>LiReddit</Heading>
-        <NextLink href="/create-post">
-          <Link ml="auto">Create post</Link>
-        </NextLink>
-      </Flex>
-      <br />
       {!data && fetching ? (
         <div>loading...</div>
       ) : (
